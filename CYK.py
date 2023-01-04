a883e31fed199b5745d04658153a513944fdad4e
Christos Kormaris <chriskormaris@gmail.com>
2023-01-04 21:13:48 +0200

    Code maintenance
    Update `CYK.py`.

--- CYK.py
+++ CYK.py
@@ -9,8 +9,6 @@ pd.set_option('display.width', 200)
 pd.set_option('max_colwidth', 60)
 
 
-# FUNCTIONS #
-
 def convert_tuples_to_lists(list_of_tuples):
     list_of_lists = [list(x) for x in list_of_tuples]
     return list_of_lists
@@ -20,39 +18,39 @@ def convert_tuples_to_lists(list_of_tuples):
 # https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
 def convert_pos_tags_to_simpler_tags(tagged_sentence):
     for i in range(len(tagged_sentence)):
-        if tagged_sentence[i][1] == 'NN'\
+        if tagged_sentence[i][1] == 'NN' \
                 or tagged_sentence[i][1] == 'NNS':
             tagged_sentence[i][1] = 'N'  # Noun
-        elif tagged_sentence[i][1] == 'NNP'\
+        elif tagged_sentence[i][1] == 'NNP' \
                 or tagged_sentence[i][1] == 'NNPS':
             tagged_sentence[i][1] = 'PN'  # Proper Noun
-        elif tagged_sentence[i][1] == 'PRP'\
-                or tagged_sentence[i][1] == 'PRP$'\
-                or tagged_sentence[i][1] == 'WP'\
+        elif tagged_sentence[i][1] == 'PRP' \
+                or tagged_sentence[i][1] == 'PRP$' \
+                or tagged_sentence[i][1] == 'WP' \
                 or tagged_sentence[i][1] == 'WP$':
             # tagged_sentence[i][1] = 'Pron'  # Pronoun
             tagged_sentence[i][1] = 'NP'
-        elif tagged_sentence[i][1] == 'VB'\
-                or tagged_sentence[i][1] == 'VBD'\
-                or tagged_sentence[i][1] == 'VBG'\
-                or tagged_sentence[i][1] == 'VBN'\
-                or tagged_sentence[i][1] == 'VBP'\
-                or tagged_sentence[i][1] == 'VBZ'\
+        elif tagged_sentence[i][1] == 'VB' \
+                or tagged_sentence[i][1] == 'VBD' \
+                or tagged_sentence[i][1] == 'VBG' \
+                or tagged_sentence[i][1] == 'VBN' \
+                or tagged_sentence[i][1] == 'VBP' \
+                or tagged_sentence[i][1] == 'VBZ' \
                 or tagged_sentence[i][1] == 'MD':
             tagged_sentence[i][1] = 'V'  # Verb
-        elif tagged_sentence[i][1] == 'DT'\
+        elif tagged_sentence[i][1] == 'DT' \
                 or tagged_sentence[i][1] == 'WDT':
             tagged_sentence[i][1] = 'Det'  # Determiner
-        elif tagged_sentence[i][1] == 'JJ'\
-                or tagged_sentence[i][1] == 'JJR'\
+        elif tagged_sentence[i][1] == 'JJ' \
+                or tagged_sentence[i][1] == 'JJR' \
                 or tagged_sentence[i][1] == 'JJS':
             tagged_sentence[i][1] = 'Adj'  # Adjective
         elif tagged_sentence[i][1] == 'IN' \
                 or tagged_sentence[i][1] == 'TO':
             tagged_sentence[i][1] = 'Prep'  # Preposition
-        elif tagged_sentence[i][1] == 'RB'\
-                or tagged_sentence[i][1] == 'RBR'\
-                or tagged_sentence[i][1] == 'RBS'\
+        elif tagged_sentence[i][1] == 'RB' \
+                or tagged_sentence[i][1] == 'RBR' \
+                or tagged_sentence[i][1] == 'RBS' \
                 or tagged_sentence[i][1] == 'WRB':
             tagged_sentence[i][1] = 'Adv'  # Adverb
         elif tagged_sentence[i][1] == 'CC':
@@ -150,15 +148,15 @@ def get_head_child(non_terminal1, non_terminal2):
 def cyk(tagged_sentence, rules):
     n = len(tagged_sentence)
 
-    C = [['-'] * (n+1) for _ in range(n)]  # n x n+1
-    rule_array = [[''] * (n+1) for _ in range(n)]  # n x n+1
+    C = [['-'] * (n + 1) for _ in range(n)]  # n x n+1
+    rule_array = [[''] * (n + 1) for _ in range(n)]  # n x n+1
 
     # initialize C
     for i in range(len(C)):
         for j in range(len(C[0])):
-            if i == j-1:
-                C[i][j] = tagged_sentence[j-1][1]
-                rule_array[i][j] = tagged_sentence[j-1][1] + '#' + str(tagged_sentence[j-1][0])
+            if i == j - 1:
+                C[i][j] = tagged_sentence[j - 1][1]
+                rule_array[i][j] = tagged_sentence[j - 1][1] + '#' + str(tagged_sentence[j - 1][0])
 
     '''
     print('Initial C:')
@@ -175,9 +173,9 @@ def cyk(tagged_sentence, rules):
                 # print('k: ' + str(k) + ', l: ' + str(k) + ', i: ' + str(i))
 
                 try:
-                    rowcol = i+l+1
+                    rowcol = i + l + 1
                     non_terminal1 = C[i][rowcol]  # in the same row as C[i][i+k+2]
-                    non_terminal2 = C[rowcol][i+k+2]  # in the same col as C[i][i+k+2]
+                    non_terminal2 = C[rowcol][i + k + 2]  # in the same col as C[i][i+k+2]
                     # rules list contains Chomsky Normal Form Grammar rules
                     parent = get_cnf_parent(rules, non_terminal1, non_terminal2)
 
@@ -187,35 +185,36 @@ def cyk(tagged_sentence, rules):
                     if parent != '-':  # find the subsentences
                         for col in range(i, rowcol):
                             subsentence1.append(tagged_sentence[col][0])
-                        for col in range(rowcol, i+k+2):
+                        for col in range(rowcol, i + k + 2):
                             subsentence2.append(tagged_sentence[col][0])
-                    
+
                     # Head child check. Useful for backtracking.
                     head_child = get_head_child(non_terminal1, non_terminal2)
-                    
+
                     # assign the parent and the rule on the arrays C and rule_array respectively
-                    if C[i][i+k+2] == '-':
-                        C[i][i+k+2] = parent
-                        rule_array[i][i+k+2] = parent + '#' + str(non_terminal1) + '#' + str(subsentence1) + '#' \
-                            + str(non_terminal2) + '#' + str(subsentence2) + '#' + str(head_child) + '#' + str(rowcol)
+                    if C[i][i + k + 2] == '-':
+                        C[i][i + k + 2] = parent
+                        rule_array[i][i + k + 2] = parent + '#' + str(non_terminal1) + '#' + str(subsentence1) + '#' \
+                                                   + str(non_terminal2) + '#' + str(subsentence2) + '#' + str(
+                            head_child) + '#' + str(rowcol)
 
                     # If the current element is the top right element (the one that will be returned)
                     # and the parent rule is 'S' (means the sentence can be produced by grammar), then stop here.
-                    if i == 0 and i+k+2 == len(C) and parent == 'S':
+                    if i == 0 and i + k + 2 == len(C) and parent == 'S':
                         pass  # do nothing, leave the array as is
 
                     # If the C[i][i+k+2] has already a symbol value and the new parent symbol disagrees,
                     # then find out which symbol has higher priority (by doing a head child check).
-                    elif parent != '-' and C[i][i+k+2] != parent:
+                    elif parent != '-' and C[i][i + k + 2] != parent:
                         previous_parent = int(rule_array[i][i + k + 2].split('#')[5])
                         ambiguous_parent = get_head_child(previous_parent, parent)
                         if ambiguous_parent == 1:
                             pass  # do nothing, leave the array as is
                         elif ambiguous_parent == 2:
-                            C[i][i+k+2] = parent
+                            C[i][i + k + 2] = parent
                             rule_array[i][i + k + 2] = parent + '#' + str(non_terminal1) + '#' \
-                                + str(subsentence1) + '#' + str(non_terminal2) + '#' \
-                                + str(subsentence2) + '#' + str(head_child) + '#' + str(rowcol)
+                                                       + str(subsentence1) + '#' + str(non_terminal2) + '#' \
+                                                       + str(subsentence2) + '#' + str(head_child) + '#' + str(rowcol)
                 except IndexError:
                     # if the indices k, l or i exceed the C array dimensions then do nothing
                     pass
@@ -234,13 +233,13 @@ def cyk(tagged_sentence, rules):
     print('')
     '''
 
-    # Run a back-track algorithm to find the syntax tree rules.
-    print('syntax tree: ')
-    print('(the head children are underlined)\n')
-    print_syntax_tree(i=0, j=len(C), rule_array=rule_array, depth=0)
-    print('')
-
     if 'S' in str(C[0][len(C)]):
+        # Run a back-track algorithm to find and print the syntax tree rules.
+        print('syntax tree: ')
+        print('(the head children are underlined)\n')
+        print_syntax_tree(i=0, j=len(C), rule_array=rule_array, depth=0)
+        print('')
+
         return True
     else:
         return False
@@ -301,43 +300,44 @@ def print_syntax_tree(i, j, rule_array, depth, max_depth=10):
         pass
 
 
-###############
+if __name__ == '__main__':
+    xml_grammar = './grammar.xml'
 
-# MAIN #
+    sentence = 'Mary had a little lamb'
+    # sentence = 'Give a good speech'
+    # sentence = 'Want a morning flight'
+    # sentence = 'She eats a fish with a fork'
+    # sentence = 'Every passenger wants a cheap flight'
+    # sentence = 'The car is a vehicle'
 
-xml_grammar = './grammar.xml'
+    # Sentences that can't be produced by the grammar.
+    # sentence = 'I Want a morning flight'
+    # sentence = 'I like trees'
+    # sentence = 'The car is red'
 
-# sentence = 'Mary had a little lamb'
-# sentence = 'Give a good speech'  # returns the same results as 'thelw mia prwini ptisi' in Greek
-# sentence = 'She eats a fish with a fork'
-# sentence = 'I like trees'
-# sentence = 'Every passenger wants a cheap flight'
-sentence = 'The car is red'
-# sentence = 'The car is a vehicle'
+    # tokenize the sentence
+    tokenized_sentence = nltk.word_tokenize(sentence)
+    print('tokenized sentence: ' + str(tokenized_sentence))
 
-# tokenize the sentence
-tokenized_sentence = nltk.word_tokenize(sentence)
-print('tokenized sentence: ' + str(tokenized_sentence))
+    # tag the sentence with POS tags
+    tagged_sentence = list(nltk.pos_tag(tokenized_sentence))
+    print('POS tags sentence: ' + str(tagged_sentence))
 
-# tag the sentence with POS tags
-tagged_sentence = list(nltk.pos_tag(tokenized_sentence))
-print('POS tags sentence: ' + str(tagged_sentence))
+    # convert 'tagged_sentence' from a list of tuples to a list of lists
+    tagged_sentence = convert_tuples_to_lists(tagged_sentence)
 
-# convert 'tagged_sentence' from a list of tuples to a list of lists
-tagged_sentence = convert_tuples_to_lists(tagged_sentence)
+    # convert POS tags to simpler, more general tags
+    tagged_sentence = convert_pos_tags_to_simpler_tags(tagged_sentence)
+    print('simplified tags sentence: ' + str(tagged_sentence))
 
-# convert POS tags to simpler, more general tags
-tagged_sentence = convert_pos_tags_to_simpler_tags(tagged_sentence)
-print('simplified tags sentence: ' + str(tagged_sentence))
+    # read grammar rules from '.xml' file
+    rules = parse_grammar(xml_grammar)
 
-# read grammar rules from '.xml' file
-rules = parse_grammar(xml_grammar)
+    # run cyk algorithm
+    # to find out if the sentence can be produced by grammar
+    produced = cyk(tagged_sentence, rules)
 
-# run cyk algorithm
-# to find out if the sentence can be produced by grammar
-produced = cyk(tagged_sentence, rules)
-
-if produced:
-    print('The sentence "' + sentence + '" can be produced by the grammar!')
-else:
-    print('The sentence "' + sentence + '" CAN\'t be produced by the grammar!')
+    if produced:
+        print('The sentence "' + sentence + '" can be produced by the grammar!')
+    else:
+        print('The sentence "' + sentence + '" CAN\'t be produced by the grammar!')
